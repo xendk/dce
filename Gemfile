a80eb0926f961e90cb0aa28e2b61b5b122202432
Thomas Fini Hansen <xen@xen.dk>
2022-09-14 20:45:35 +0200

    Fix up SimpleCov

--- Gemfile
+++ Gemfile
@@ -6,5 +6,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'solargraph', group: :development
 gem 'simplecov', group: :development
+gem 'simplecov-cobertura', group: :development
 
 gem "cucumber", "~> 5.2"
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,14 +5,5 @@ ENV['PATH'] = "#{File.join(Dir.pwd, 'bin')}:#{ENV['PATH']}"
 
 ENV['COVERAGE'] = '1' if ENV['GITHUB_ACTION']
 
-if ENV['COVERAGE']
-  at_exit do
-    require 'simplecov'
-    SimpleCov.start
-
-    if ENV['GITHUB_ACTION']
-      require 'simplecov-cobertura'
-      SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
-    end
-  end
-end
+# See .simplecov file for setup.
+require 'simplecov' if ENV['COVERAGE']
